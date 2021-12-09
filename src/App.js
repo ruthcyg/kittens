3028438d2f0ff5c963f40745e1a4e60d2794871e
Ruthcy G <ewho.ruth2014@gmail.com>
2021-12-09 15:19:03 +0530

    add classes and component in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//function App() {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+        { name: "Dracula" },
+        { name: "Zombie" },
+        { name: "Frankenstein" },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p> {this.state.string}</p>
+          <button onClick={() => this.setState({ string: "Hello Husband" })}>
+            change text{" "}
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;
