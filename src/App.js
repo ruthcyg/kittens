c8a6b730837d42f48685ec6602e41de3842f9df3
Ruthcy G <ewho.ruth2014@gmail.com>
2021-12-21 09:35:14 +0530

    complete kitten project

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 //import React from "react";
 
 import "./App.css";
-
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 //function App() {
 //class App extends React.Component {
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField: " ",
     };
   }
   componentDidMount() {
@@ -18,12 +20,21 @@ class App extends Component {
       .then((users) => this.setState({ monsters: users }));
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <SearchBox
+          placeholder="search kittens"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
